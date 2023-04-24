7e1b25a410408660fe2c6472c0f5fe61ea730cd3
Payson Parker <payson.parker@comcast.net>
2023-04-23 17:46:18 -0700

    Delete product and edit product

--- src/DeleteProduct.js
+++ src/DeleteProduct.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import dataSource from './dataSource';
 import { useNavigate } from 'react-router-dom';
 
@@ -6,16 +6,36 @@ const DeleteProduct = (props) => {
 
     const navigate = useNavigate();
 
+    let productId = window.location.pathname.substring(8);
+
+    const handleFormSubmit = (event) => {
+        event.preventDefault();
+
+        console.log("submit");
+            
+        deleteProduct();
+    };
+
     const deleteProduct = async (product) => {
-        const response = await dataSource.delete(`/products/${props.match.params.productId}`);
+        console.log('productId', productId);
+        const response = await dataSource.delete(`/products/${productId}`);
         console.log(response);
         console.log(response.data);
         navigate("/");
     };
-    deleteProduct();
+
+    const handleCancel = () => {
+        navigate("/");
+    };
 
     return (
-        <div>Delete page</div>
+        <div className='container'>
+            <h1>Are you sure you want to delete this post?</h1>
+            <form onSubmit={handleFormSubmit}>
+                <button type="button" className="btn btn-light" onClick={handleCancel}>Cancel</button>
+                <button type="submit" className="btn btn-primary">Confirm</button>
+            </form>
+        </div>
     );
 };
 
